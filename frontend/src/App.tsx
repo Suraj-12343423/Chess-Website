e4cf8332f23941721cd0af7b02ab17312910b8fa
Sai Suraj <surajkalla809@gmail.com>
2024-06-20 14:18:40 +0530

    CU

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState } from "react";
+
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Game } from "./PAGES/Game";
